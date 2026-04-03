ecbe0e1157cbf042cf8c442496c01fa51e6459ed
vida65979-arch <vida65979@gmail.com>
2026-04-03 13:08:13 +0600

    Add solution - CodeHub

---
+++ 8-kyu/java/reversed-strings/reversed-strings.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Kata {
+​
+  public static String solution(String str) {
+        List<String> list = new ArrayList<>(List.of(str.split("")));
+        Collections.reverse(list);
+        return (String.join("", list));
+  }
+​
+}
